3db2a7d9c37f86f18993bf906e309702968109ef
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2022-04-30 00:15:24 -0600

    Diseño reservacion, periodoreserva, localevento

---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/PeriodoReserva/PeriodoReserva.java
@@ -0,0 +1,4 @@
+package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Carolina.PeriodoReserva;
+
+public class PeriodoReserva {
+}
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/PeriodoReserva/PeriodoReservaConsultarActivity.java
@@ -0,0 +1,16 @@
+package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Carolina.PeriodoReserva;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
+
+public class PeriodoReservaConsultarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_periodo_reserva_consultar);
+    }
+}

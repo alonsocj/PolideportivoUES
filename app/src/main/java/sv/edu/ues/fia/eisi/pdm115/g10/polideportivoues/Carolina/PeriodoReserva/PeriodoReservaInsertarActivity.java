7b43bbb862c4331e76706cb15fc2f9284d221e56
CarolinaIPD <75870831+CarolinaIPD@users.noreply.github.com>
2022-04-30 22:10:07 -0600

    cruds de resrvacion, periodoreserva, localevento

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/PeriodoReserva/PeriodoReservaInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/PeriodoReserva/PeriodoReservaInsertarActivity.java
@@ -2,15 +2,68 @@ package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Carolina.PeriodoReserva;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
+import java.util.Calendar;
+
+import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Carolina.Reservacion.ReservacionInsertarActivity;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
 
 public class PeriodoReservaInsertarActivity extends AppCompatActivity {
+
+    EditText et_FechaI;
+    EditText et_FechaF;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_periodo_reserva_insertar);
+
+        //Mostrar Calendario
+
+        et_FechaI=findViewById(R.id.et_FechaI);
+        et_FechaF=findViewById(R.id.et_FechaF);
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+        String date=day+"/"+(month+1)+"/"+year;
+        et_FechaI.setText(date);
+        et_FechaF.setText(date);
+
+        et_FechaI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(PeriodoReservaInsertarActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        String date=dayOfMonth+"/"+month+"/"+year;
+                        et_FechaI.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        et_FechaF.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(PeriodoReservaInsertarActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        String date=dayOfMonth+"/"+month+"/"+year;
+                        et_FechaF.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        });
+        //Fin calendario
     }
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/Reservacion/ReservacionInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Carolina/Reservacion/ReservacionInsertarActivity.java
@@ -3,13 +3,61 @@ package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Carolina.Reservacion;
 
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10;
+import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.MainActivity;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
-public class ReservacionInsertarActivity extends AppCompatActivity {
+public class ReservacionInsertarActivity extends AppCompatActivity{
+
+    EditText etFechaR;
+    DatePickerDialog.OnDateSetListener setListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservacion_insertar);
+
+    //Mostrar Calendario
+        etFechaR=findViewById(R.id.et_FechaR);
+        Calendar calendar=Calendar.getInstance();
+        final int year=calendar.get(Calendar.YEAR);
+        final int month=calendar.get(Calendar.MONTH);
+        final int day=calendar.get(Calendar.DAY_OF_MONTH);
+        String date=day+"/"+(month+1)+"/"+year;
+        etFechaR.setText(date);
+
+        etFechaR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatePickerDialog datePickerDialog=new DatePickerDialog(ReservacionInsertarActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+                        month=month+1;
+                        String date=dayOfMonth+"/"+month+"/"+year;
+                        etFechaR.setText(date);
+                    }
+                },year,month,day);
+                datePickerDialog.show();
+            }
+        }); //Fin calendario
+
     }
+
 }

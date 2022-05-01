8ce840b15d426f3c2ef4f25692ab9f5202cfab79
ChristianG0816 <chrisgue0819@gmail.com>
2022-05-01 17:59:09 -0600

    Se completa la tabla evento, queda pendiente la validación de campos vacios y que en ves de mostrar el identificador en consultar se muestre el nombre como tal.

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Chris/Evento/EventoConsultarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Chris/Evento/EventoConsultarActivity.java
@@ -1,5 +1,8 @@
 package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Chris.Evento;
 
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +10,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.lang.reflect.Array;
 
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
@@ -16,7 +21,11 @@ public class EventoConsultarActivity extends AppCompatActivity {
     EditText editIdEven, editIdTE, editNom, editCost;
     Button consultarEvent;
     ControlBDG10 helper;
+    private SQLiteDatabase db;
+    String nombreTipoEvento[];
+    String idEvento[];
 
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +38,18 @@ public class EventoConsultarActivity extends AppCompatActivity {
 
         consultarEvent = findViewById(R.id.botonConsultarEvent);
 
+        /*Obtención de datos de la base de datos*/
+        db = openOrCreateDatabase("polideportivo.s3db",SQLiteDatabase.CREATE_IF_NECESSARY,null);
+        Cursor cursor = db.rawQuery("SELECT * FROM evento INNER JOIN tipoevento ON evento.idTipoE == tipoevento.idTipoE",null);
+        nombreTipoEvento = new String[cursor.getCount()];
+        idEvento = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for(int i=0; i<nombreTipoEvento.length; i++){
+            idEvento[i] = cursor.getString(0); //Id de los eventos (Tabla evento)
+            nombreTipoEvento[i] = cursor.getString(5); //Nombre Evento (Tabla tipoevento)
+            cursor.moveToNext();
+        }
+
         consultarEvent.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -38,6 +59,11 @@ public class EventoConsultarActivity extends AppCompatActivity {
                 if(evento == null){
                     Toast.makeText(EventoConsultarActivity.this, "No existe el evento", Toast.LENGTH_SHORT).show();
                 }else{
+                  /*  for (int i=0; i< idEvento.length; i++){
+                        if(editIdEven.getText().toString() == idEvento[i]){
+                            editIdTE.setText(nombreTipoEvento[i]);
+                        }
+                    }*/
                     editIdTE.setText(evento.getIdTipoE());
                     editNom.setText(evento.getNomEvento());
                     editCost.setText(String.valueOf(evento.getCostoEvento()));

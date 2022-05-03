507b418b5761859de957663e7e5db75b9149edaf
alonsocj <cornejo.aj98@hotmail.com>
2022-05-03 15:05:14 -0600

    Funcionalidades de la parte de cobro completas

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/Cobro/CobroInsertarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/Cobro/CobroInsertarActivity.java
@@ -1,17 +1,23 @@
 package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Alonso.Cobro;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
 
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10Alonso;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
@@ -22,6 +28,7 @@ public class CobroInsertarActivity extends AppCompatActivity {
     //TextInputLayout inputIdCobro, inputIdPago, inputDuracion, inputPrecio;
     TextInputEditText editIdCobro, editCantPersonas, editDuracion, editPrecio;
     MaterialAutoCompleteTextView editTipoPago;
+    LinearLayout linearLayout;
     Button btnInsertar;
     SQLiteDatabase db;
     String[] arridTipoPago;
@@ -38,14 +45,56 @@ public class CobroInsertarActivity extends AppCompatActivity {
         editPrecio = findViewById(R.id.editText_precio);
         editTipoPago = findViewById(R.id.list_tipo_pago);
         btnInsertar = findViewById(R.id.button_guardar);
+        linearLayout = findViewById(R.id.linearLayout);
 
+        linearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hideKeyboard(v);
+            }
+        });
+        editDuracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+                        .setTimeFormat(TimeFormat.CLOCK_24H)
+                        .setHour(1)
+                        .setMinute(0)
+                        .setTitleText(R.string.time_picker_title)
+                        .setInputMode(MaterialTimePicker.INPUT_MODE_CLOCK)
+                        .build();
+                timePicker.show(getSupportFragmentManager(), "TIME_PICKER");
+                timePicker.addOnPositiveButtonClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (timePicker.getHour() < 10) {
+                            if (timePicker.getMinute() < 10) {
+                                String duracion = "0" + timePicker.getHour() + ":0" + timePicker.getMinute();
+                                editDuracion.setText(duracion);
+                            } else {
+                                String duracion = "0" + timePicker.getHour() + ":" + timePicker.getMinute();
+                                editDuracion.setText(duracion);
+                            }
+                        } else {
+                            if (timePicker.getMinute() < 10) {
+                                String duracion = timePicker.getHour() + ":0" + timePicker.getMinute();
+                                editDuracion.setText(duracion);
+                            } else {
+                                String duracion = timePicker.getHour() + ":" + timePicker.getMinute();
+                                editDuracion.setText(duracion);
+                            }
+                        }
+                    }
+                });
+            }
+        });
         // obtener datos de la lista de tipo pago
-        db = openOrCreateDatabase("polideportivo.s3db",MODE_PRIVATE,null);
-        Cursor cursor = db.rawQuery("SELECT * FROM tipopago",null);
+        db = openOrCreateDatabase("polideportivo.s3db", MODE_PRIVATE, null);
+        Cursor cursor = db.rawQuery("SELECT * FROM tipopago", null);
         arridTipoPago = new String[cursor.getCount()];
         arrtipoPago = new String[cursor.getCount()];
         cursor.moveToFirst();
-        for (int i=0; i<cursor.getCount(); i++){
+        for (int i = 0; i < cursor.getCount(); i++) {
             arridTipoPago[i] = cursor.getString(0);
             arrtipoPago[i] = cursor.getString(1);
             cursor.moveToNext();
@@ -54,33 +103,48 @@ public class CobroInsertarActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrtipoPago);
         editTipoPago.setAdapter(adapter);
 
-        btnInsertar.setOnClickListener(new View.OnClickListener(){
+        btnInsertar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String eventosRegistrados;
-                int idCobro = Integer.parseInt(editIdCobro.getText().toString());
-                String tipoPago = editTipoPago.getText().toString();
-                String idTipoPago = "";
-                int cantPersonas = Integer.parseInt(editCantPersonas.getText().toString());
-                float duracion = Float.parseFloat(editDuracion.getText().toString());
-                float precio = Float.parseFloat(editPrecio.getText().toString());
-                for(int i=0; i<arrtipoPago.length; i++){
-                    if(tipoPago.equals(arrtipoPago[i])){
-                        idTipoPago = arridTipoPago[i];
+                try {
+
+                    hideKeyboard(v);
+
+                    String eventosRegistrados;
+                    int idCobro = Integer.parseInt(editIdCobro.getText().toString());
+                    String tipoPago = editTipoPago.getText().toString();
+                    String idTipoPago = "";
+                    int cantPersonas = Integer.parseInt(editCantPersonas.getText().toString());
+                    String duracion = editDuracion.getText().toString();
+                    float precio = (Float) Float.valueOf(editPrecio.getText().toString());
+
+                    for (int i = 0; i < arrtipoPago.length; i++) {
+                        if (tipoPago.equals(arrtipoPago[i])) {
+                            idTipoPago = arridTipoPago[i];
+                        }
                     }
+                    Cobro cobro = new Cobro();
+                    cobro.setIdCobro(idCobro);
+                    cobro.setIdPago(idTipoPago);
+                    cobro.setCantPersonas(cantPersonas);
+                    cobro.setDuracion(duracion);
+                    cobro.setPrecio(precio);
+                    helper.open();
+                    eventosRegistrados = helper.insertar(cobro);
+                    helper.close();
+                    Toast.makeText(getApplicationContext(), eventosRegistrados, Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(getApplicationContext(), "Error al insertar", Toast.LENGTH_SHORT).show();
                 }
-                Cobro cobro = new Cobro();
-                cobro.setIdCobro(idCobro);
-                cobro.setIdPago(idTipoPago);
-                cobro.setCantPersonas(cantPersonas);
-                cobro.setDuracion(duracion);
-                cobro.setPrecio(precio);
-                helper.open();
-                eventosRegistrados = helper.insertar(cobro);
-                helper.close();
-                Toast.makeText(getApplicationContext(), eventosRegistrados, Toast.LENGTH_SHORT).show();
 
             }
         });
+    }
+
+    public void hideKeyboard(View view) {
+        if (getCurrentFocus() != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(CobroInsertarActivity.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }

c2114588c87f61a231d79cdb85476fa1c18033e0
alonsocj <cornejo.aj98@hotmail.com>
2022-05-04 23:18:44 -0600

    - se cambioo la interfaz de tipo evento - se soluciono un error al eliminar cobro

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/TipoEvento/TipoEventoActualizarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/TipoEvento/TipoEventoActualizarActivity.java
@@ -7,31 +7,37 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10Alonso;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
 
 public class TipoEventoActualizarActivity extends AppCompatActivity {
 
     ControlBDG10Alonso helper;
-    EditText editIdTipoEvento;
-    EditText editNombreTipoEvento;
+    TextInputEditText editIdTipoEvento;
+    TextInputEditText editNombreTipoEvento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_evento_actualizar);
         helper = new ControlBDG10Alonso(this);
-        editIdTipoEvento = (EditText) findViewById(R.id.editText_id);
-        editNombreTipoEvento = (EditText) findViewById(R.id.editText_nombre);
+        editIdTipoEvento = findViewById(R.id.editText_idTipoE);
+        editNombreTipoEvento = findViewById(R.id.EditTipoE);
     }
 
     public void actualizarTipoEvento(View view) {
-        TipoEvento tipoEvento = new TipoEvento();
-        tipoEvento.setIdTipoE(editIdTipoEvento.getText().toString());
-        tipoEvento.setNombreTipoE(editNombreTipoEvento.getText().toString());
-        helper.open();
-        String estado = helper.actualizar(tipoEvento);
-        helper.close();
-        Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+        try {
+            TipoEvento tipoEvento = new TipoEvento();
+            tipoEvento.setIdTipoE(editIdTipoEvento.getText().toString());
+            tipoEvento.setNombreTipoE(editNombreTipoEvento.getText().toString());
+            helper.open();
+            String estado = helper.actualizar(tipoEvento);
+            helper.close();
+            Toast.makeText(this, estado, Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(this, "Error al actualizar el tipo de evento", Toast.LENGTH_SHORT).show();
+        }
     }
 }
--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/TipoEvento/TipoEventoConsultarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/TipoEvento/TipoEventoConsultarActivity.java
@@ -7,32 +7,38 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10Alonso;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
 
 public class TipoEventoConsultarActivity extends AppCompatActivity {
 
     ControlBDG10Alonso helper;
-    EditText editIdTipoEvento;
-    EditText editNombreTipoEvento;
+    TextInputEditText editIdTipoEvento;
+    TextInputEditText editNombreTipoEvento;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tipo_evento_consultar);
         helper = new ControlBDG10Alonso(this);
-        editIdTipoEvento = (EditText) findViewById(R.id.editText_id);
-        editNombreTipoEvento = (EditText) findViewById(R.id.editText_nombre);
+        editIdTipoEvento = findViewById(R.id.editText_idTipoE);
+        editNombreTipoEvento = findViewById(R.id.EditTipoE);
     }
 
-    public void consultarTipoEvento(View view){
-        helper.open();
-        TipoEvento tipoEvento = helper.consultarTipoEvento(editIdTipoEvento.getText().toString());
-        helper.close();
-        if(tipoEvento == null){
-            Toast.makeText(this, "Tipo de evento con id '"+editIdTipoEvento.getText().toString()+ "' no encontrado", Toast.LENGTH_SHORT).show();
-        }else{
-            editNombreTipoEvento.setText(tipoEvento.getNombreTipoE());
+    public void consultarTipoEvento(View view) {
+        try {
+            helper.open();
+            TipoEvento tipoEvento = helper.consultarTipoEvento(editIdTipoEvento.getText().toString());
+            helper.close();
+            if (tipoEvento == null) {
+                Toast.makeText(this, "Tipo de evento con id '" + editIdTipoEvento.getText().toString() + "' no encontrado", Toast.LENGTH_SHORT).show();
+            } else {
+                editNombreTipoEvento.setText(tipoEvento.getNombreTipoE());
+            }
+        } catch (Exception e) {
+            Toast.makeText(this, "Error al consultar el tipo de evento", Toast.LENGTH_SHORT).show();
         }
     }
 }

7afb03fd9f71feaec5db36a5ba4e6f9b47fc2559
alonsocj <cornejo.aj98@hotmail.com>
2022-05-06 09:58:48 -0600

    SplashScreen agregada (foto actual no es la final) - se completo las funcionalidades de la clase dia

--- app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/Dia/DiaEliminarActivity.java
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/Alonso/Dia/DiaEliminarActivity.java
@@ -1,16 +1,50 @@
 package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.Alonso.Dia;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.ControlBDG10Alonso;
 import sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues.R;
 
 public class DiaEliminarActivity extends AppCompatActivity {
+
+    ControlBDG10Alonso helper;
+    TextInputEditText editNomDia;
+    Button btnEliminar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dia_eliminar);
+        helper = new ControlBDG10Alonso(this);
+        editNomDia = (TextInputEditText) findViewById(R.id.editText_nom_dia);
+        btnEliminar = (Button) findViewById(R.id.button_eliminar);
+
+        btnEliminar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+
+
+                    String regEliminado;
+                    Dia dia = new Dia();
+                    dia.setNombreDia(editNomDia.getText().toString());
+                    helper.open();
+                    regEliminado = helper.eliminar(dia);
+                    helper.close();
+                    Toast.makeText(getApplicationContext(), regEliminado, Toast.LENGTH_SHORT).show();
+                } catch (Exception e) {
+                    Toast.makeText(DiaEliminarActivity.this, "Error al eliminar", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/sv/edu/ues/fia/eisi/pdm115/g10/polideportivoues/SplashScreen.java
@@ -0,0 +1,23 @@
+package sv.edu.ues.fia.eisi.pdm115.g10.polideportivoues;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
